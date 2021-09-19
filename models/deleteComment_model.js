7a7fe4cb96b3bd7a533abe6bfc9f5c241df4657e
JianShan-1214 <l059069045@gmail.com>
2021-09-19 17:25:00 +0800

    refactor models which mixed with controller logic

--- models/deleteComment_model.js
+++ models/deleteComment_model.js
@@ -2,31 +2,13 @@ const mongoose = require("mongoose");
 
 const CourseRate = require("./rating_model");
 
-module.exports = function uploadComments(req) {
-  let isDone = false;
+module.exports = function uploadComments(findObj,dataObj) {
   return new Promise(function(resolve, reject) {
-    let rateHW = req.body.作業量;
-    let rateLN = req.body.豐富度;
-    let rateRD = req.body.推薦度;
-    if (rateHW===undefined) rateHW=0
-    if (rateLN===undefined) rateLN=0
-    if (rateRD===undefined ) rateRD=0
-    if (req.isAuthenticated())
       CourseRate.updateOne(
-        { teacher: req.params.teacher, subject: req.params.subject, userID: req.session.passport.user , isHidden: false},
+        findObj,dataObj
         // form 在body裡，在網址的是params！！！
         // form 在body裡，在網址的是params！！！
         // form 在body裡，在網址的是params！！！
-        {
-          isHidden: true,
-          modifiedAt : Date.now()
-        }
-      ).then(()=>{
-        resolve(isDone);
-      })
-
-    else {
-      resolve(isDone);
-    }
+      ).then(()=>{resolve()})
   });
 };
