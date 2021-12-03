abcaa33be5b435d3c3d4f9840341ebb4ff2c7ff0
fanlan1210 <fanlan1210@users.noreply.github.com>
2021-12-03 18:21:10 +0800

    fix auth problem, better SEO

--- services/auth_middleware.js
+++ services/auth_middleware.js
@@ -1,8 +1,26 @@
-
-module.exports = function checkAuth(req, res, next){
-    if(req.isAuthenticated()){
-        next();
-    }else{
-        res.redirect("/auth/login");
+class AuthMiddleware {
+  /**
+    *  This method check user's login status.
+    *  if user is not logged, it redirect user to login page.
+   */
+  authen(req, res, next) {
+    if (req.isAuthenticated()) {
+      next();
+    } else {
+      res.redirect("/auth/login");
     }
-};
+  };
+
+  /**
+    *  This method just check user's login status and return its state.
+   */
+  author(req, res, next) {
+    if (req.isAuthenticated()) {
+      next();
+    } else {
+      res.status(401).send("Unauthorized");
+    }
+  }
+}
+
+module.exports = AuthMiddleware;
