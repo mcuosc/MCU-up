0a7f9d83ef07bcd191a1153caba5266434d6b175
alanhc <alanhc.tseng1999@gmail.com>
2022-04-30 00:45:42 +0800

    Fix (#19)
    * remove mongoose-paginate-v2 depentcy
    
    * remove mongoose-paginate-v2 depentcy
    
    * fix .env example, env ?
    
    * fix prerender ? error

--- models/getCoursesList_model.js
+++ models/getCoursesList_model.js
@@ -1,7 +1,7 @@
 const mongoose = require("mongoose");
 const mongoosePaginate = require("mongoose-paginate-v2");
 
-const Course = require('./schema/course_model');
+const Courses = require('./schema/course_model');
 
 /*function escapeRegex(text) {
   return text.replace(/[-[\]{}()*+?.,\\^$|#\s]/g, "\\$&");
@@ -10,9 +10,9 @@ const Course = require('./schema/course_model');
 module.exports = function getCourseList(req) {
   let result = {};
   let campus = []
-  if ( req.query.campus === undefined || req.query.campus === '') campus = ["桃園","台北","成功","基河","金門"];
+  if (req.query.campus === undefined || req.query.campus === '') campus = ["桃園", "台北", "成功", "基河", "金門"];
   else campus = JSON.parse(req.query.campus)
-  return new Promise(function(resolve, reject) {
+  return new Promise(function (resolve, reject) {
     let page = 1;
     if (req.query.page === undefined) page = 1;
     else {
@@ -30,7 +30,39 @@ module.exports = function getCourseList(req) {
       page: page,
       limit: 16,
     };
-    Course.paginate(
+    let pageNumber = page;
+    let nPerPage = 16;
+
+    Courses.find(
+      {
+        $and: [
+          {
+            $or: [
+              { "科目.name": { $regex: search_regex } },
+              { "科目.id": { $regex: search_regex } },
+              { "任課教師.正課": { $regex: search_regex } },
+              { "任課教師.實習": { $regex: search_regex } },
+            ]
+          },
+          { "學校.校區": { $elemMatch: { $in: campus } } } //校區
+        ],
+      },
+      (err, result) => {
+        if (err) {
+          console.log(err);
+          result.status = "Fail to find data.";
+        }
+        result = { queryCourses: { docs: result }, search: search_str, campus: JSON.stringify(campus) };
+        resolve(result);
+      }
+    )
+      .sort({})
+      .skip(pageNumber > 0 ? ((pageNumber - 1) * nPerPage) : 0)
+      .limit(nPerPage);
+
+
+    //console.log(options)
+    /*Course.paginate(
       {
           $and:[
             {$or: [
@@ -49,11 +81,13 @@ module.exports = function getCourseList(req) {
           reject(err);
         } else {
           // if (search.length>1) search = search.slice(1,-1)
-
+          
           result = { queryCourses: result, search: search_str, campus:JSON.stringify(campus) };
+          console.log(result)
           resolve(result);
         }
       }
     );
+    */
   });
 };
--- services/preRenderMiddleware.js
+++ services/preRenderMiddleware.js
@@ -1,3 +1,4 @@
+require("dotenv").config();
 module.exports = function(req,res,next) {
   res.locals.isAuthenticated = req.isAuthenticated();
   res.locals.google_analytics = process.env?.GA_MEASUREMENT_ID;
